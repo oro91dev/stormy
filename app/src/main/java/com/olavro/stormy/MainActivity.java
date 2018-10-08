3224b780fb0d3b3f4110fd44e4c68c9da6b6bd7c
oro91dev <oro91dev@gmail.com>
2018-10-08 18:25:39 +0200

    75% done

--- app/src/main/java/com/olavro/stormy/MainActivity.java
+++ app/src/main/java/com/olavro/stormy/MainActivity.java
@@ -1,13 +1,83 @@
 package com.olavro.stormy;
 
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+import okhttp3.*;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "6b54f87ef6f0aa7a53451775c46d0a86";
+
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+        String forecastURL = "https://api.darksky.net/forecast/" + apiKey + "/" + latitude +"," + longitude;
+
+        if(isNetworkAvailable()) {
+
+            OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder()
+                    .url("https://publicobject.com/helloworld.txt")
+                    .build();
+
+            Call call = client.newCall(request);
+
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    try {
+                        Log.v(TAG, response.body().string());
+                        if (response.isSuccessful()) {
+                        } else {
+
+                        }
+                    } catch (IOException e) {
+                        alertUserAboutError();
+                    }
+                }
+            });
+        }
+        Log.d(TAG, "Main UI code is running, horray!");
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+        boolean isAvailable = false;
+
+        if(networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        }
+        else {
+            Toast.makeText(this, R.string.network_unavailable_message, Toast.LENGTH_LONG).show();
+        }
+        return  isAvailable;
+    }
+
+    @SuppressWarnings("deprecation")
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getSupportFragmentManager(), "error_dialog");
     }
 }
